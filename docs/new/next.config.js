fdba6651b55fd3092fbe9831044e89fef3e71554
adonisarun123 <adonis.arun@gmail.com>
2025-06-19 19:51:49 +0530

    Remove remark dependencies and fix configuration

---
+++ docs/new/next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  reactStrictMode: true,
+  typescript: { ignoreBuildErrors: true },
+}; 
--- next.config.js
+++ next.config.js
@@ -1,14 +1,1 @@
-/** @type {import('next').NextConfig} */
-const withMDX = require('@next/mdx')({
-  extension: /\.(md|mdx)$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
-
-module.exports = withMDX({
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  reactStrictMode: true,
-  typescript: { ignoreBuildErrors: true },
-}); 
+/** @type {import("next").NextConfig} */ module.exports = { pageExtensions: ["ts", "tsx", "js", "jsx"], reactStrictMode: true, typescript: { ignoreBuildErrors: true } };
